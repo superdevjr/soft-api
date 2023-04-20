4c4b739bfe2eb83a57d885744077af6f26f028ed
jhylacey <jhylacey@proton.me>
2023-04-20 12:31:17 -0500

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT;
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
